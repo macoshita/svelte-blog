c6ad1be1a292a2bc2af1db2b6736d9a7d8e9a237
macoshita <macoshita@users.noreply.github.com>
2021-04-14 12:39:06 +0900

    Add link to top

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -4,7 +4,14 @@
 </script>
 
 <header>
-	<h1>@macoshita</h1>
+	<h1><a class="blog-title" href="/">@macoshita</a></h1>
 </header>
 
 <slot />
+
+<style lang="scss">
+	.blog-title {
+		color: var(--nc-tx-1);
+		text-decoration: none;
+	}
+</style>
